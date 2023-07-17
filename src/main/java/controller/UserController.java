df694c637db8a38f80b5b46242435df0a8a768c3
Yumi Kim <yumii2307@naver.com>
2023-07-17 16:01:28 +0900

    Day 1

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserController
+ */
+@WebServlet("/user/*")
+public class UserController extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] uri = request.getRequestURI().split("/");
+		String action = uri[uri.length - 1];
+		
+		RequestDispatcher rd = null;
+		switch (action) {
+		case "list":
+			rd = request.getRequestDispatcher("/WEB-INF/view/user/list.jsp");
+			rd.forward(request, response);
+			break;
+		case "login":
+			if (request.getMethod().equals("GET")) {
+				rd = request.getRequestDispatcher("/WEB-INF/view/user/login.jsp");
+				rd.forward(request, response);
+			} else {
+				
+			}
+			break;
+		}
+	}
+
+
+}
---
+++ src/main/webapp/WEB-INF/view/user/list.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<%@ include file="../common/head.jspf" %>
+</head>
+<body>
+	<%@ include file="../common/top.jspf" %>
+	
+    <div class="container" style="margin-top: 80px;">
+        <div class="row">
+        	<%@ include file="../common/aside.jspf" %>
+        
+        	<!-- ======================== main ======================== -->
+        	<div class="col-sm-9">
+        		<h3><strong>사용자 목록</strong></h3>
+        		<hr>
+        	</div>
+        </div>
+    </div>
+	
+	<%@ include file="../common/bottom.jspf" %>
+</body>
+</html>
